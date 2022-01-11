49d7ce6a46155a37a842cad2ff5f155a8cdef06f
Ananya Ghosh <75447439+A-GHOSH-dev@users.noreply.github.com>
2022-01-11 21:38:43 +0530

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+
+  let firebaseConfig = {
+    apiKey: "AIzaSyDTlREQLA94N3ktApdEhfYTHdzWwRLYWyM",
+    authDomain: "blog-webpage-fa2c3.firebaseapp.com",
+    projectId: "blog-webpage-fa2c3",
+    storageBucket: "blog-webpage-fa2c3.appspot.com",
+    messagingSenderId: "344534518772",
+    appId: "1:344534518772:web:3886c541ea9e5bd74747a1"
+  };
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
